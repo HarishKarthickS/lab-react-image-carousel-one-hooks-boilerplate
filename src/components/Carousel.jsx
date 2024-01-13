d4e835ded643e92331e3bb255562925b2dae49c5
Your Name~ <harish.s@google.com>
2024-01-13 21:03:15 +0530

    Complete

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -8,6 +8,53 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // complete the function below:
 function Carousel() {
+    let [currIndex,setCurrIndex] = useState(0);
+    console.log("img",images)
+
+    function moveBackward(){
+        if(currIndex === 0){
+            setCurrIndex(images.length-1);
+        }
+        else{
+            setCurrIndex(currIndex-1);
+        }
+    }
+
+    function moveForward(){
+        if(currIndex === images.length-1){
+            setCurrIndex(0);
+        }
+        else{
+            setCurrIndex(currIndex+1);
+        }
+    }
+
+    const myStyle ={
+        backgroundImage:`url(${images[currIndex].img})`,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        backgroundSize:"100% 100%",
+        height: "80vh",
+        width: "42vw",
+    }
+    return(
+        <div className="main-container" style={myStyle}>
+            <div className="arrow">
+                <ArrowBackIosIcon onClick={moveBackward}/>
+            </div>
+            <div className="Sub-container">
+                <div className="Content">
+                    <h2>{images[currIndex].title}</h2>
+                </div>
+                <div className="Content">
+                    <h3>{images[currIndex].subtitle}</h3>
+                </div>
+            </div>
+            <div className="arrow">
+                <ArrowForwardIosIcon onClick={moveForward}/>
+            </div> 
+        </div>
+    )
 }
 
 export default Carousel;
